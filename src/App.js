51b24082bd3ce29bf8b47a6235dc56d56d62afd9
Daniel Gading <daniel.gading@agencychief.com>
2018-08-06 14:12:52 -0400

    Final commit for part-1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import { salesreps } from './data/data';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      salesReps: salesreps,
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header title="Dunder Mifflin Paper Sales Tracker" />
+        <h2>Sales Rep List</h2>
+        <ol>
+          {salesreps.map(salesrep => (
+            <li key={salesrep.id}>
+              <h3>{salesrep.firstName} {salesrep.lastName}</h3>
+              <b>Email: </b> {salesrep.email} <br />
+              <b>Current Sales: </b> {salesrep.currentSales}
+            </li>
+          ))}
+        </ol>
       </div>
     );
   }
